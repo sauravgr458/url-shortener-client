7d3e0a16824a327df2cfde97334cbe1d298940b1
Saurav G <sauravgr458@gmail.com>
2025-05-22 01:08:51 +0530

    Removed ngrok dependency

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_BASE_URL || "http://localhost:3000/";
-const NGROK_HEADER = { "ngrok-skip-browser-warning": "true" };
 
 export const shortenUrl = async (full_url) => {
-  return axios.post(`${API_BASE}/short_urls`, { full_url }, { headers: NGROK_HEADER });
+  return axios.post(`${API_BASE}/short_urls`, { full_url });
 };
 
 export const fetchTopUrls = async () => {
-  return axios.get(`${API_BASE}/`, { headers: NGROK_HEADER });
+  return axios.get(`${API_BASE}/`);
 };
